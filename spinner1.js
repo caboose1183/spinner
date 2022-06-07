1b0b2cbf63955a070a892dd301d19c638533d4b5
Sidney Pang <sidneypang@hotmail.co.uk>
2022-06-07 17:34:12 +0000

    added spinner of character, used \r to go to beginning of line

---
+++ spinner1.js
@@ -0,0 +1,14 @@
+const spinner = "|/-\\\-|/-\\\-|/-\\\-|";
+
+let time = 0;
+
+let st = 'hello from spinner1.js... \rheyyy\n';
+
+for (const char of spinner) {
+  setTimeout(() => {
+    process.stdout.write(char + '\r');
+  }, time)
+
+  time += 200
+}
+
